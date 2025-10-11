384f13f22f7a7b724f6246110e9a5cb7d930d9f1
Gabriel-AFerreira <fatecmaua01@gmail.com>
2025-10-11 00:24:25 +0000

    primeiro commit para criação do código principal do projeto.

---
+++ pokeDex/App.tsx
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+
+interface PokemonAtributes {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+}
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
